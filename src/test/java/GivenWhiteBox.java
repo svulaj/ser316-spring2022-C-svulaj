3c3e84f69b4c43b90180058f40f285acdf6b2b31
shaun vulaj <svulaj@asu.edu>
2022-02-25 15:20:37 -0700

    in the class givenWhiteBox the test method checkoutOneBear has been altered on two lines. The lines that have been added are lines--> 32 & 36. Changes: line32: collect tax. Line36: add tax to cost of said bear. This part was done for step 2 for assignment#3

--- src/test/java/GivenWhiteBox.java
+++ src/test/java/GivenWhiteBox.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import main.java.BearWorkshop;
 
-/*
+
 public class GivenWhiteBox {
     BearWorkshop oneBear;
 
@@ -16,14 +16,25 @@ public class GivenWhiteBox {
     public void setUp() throws Exception {
     }
 
-
+/**
+ * The change that needed to happen here was that the tax was not being applied
+ * so to fix this issue i added in one line of code one line#30 to collect the tax value.
+ * and
+ * one line of code on line#36 to add the tax cost to the cost retrieved from the bear in question.
+ * 
+ */
     @Test
     public void checkoutOneBear() {
         // One Student
         oneBear = new BearWorkshop("AZ");
-        oneBear.addBear(new Bear());
-        Double ans = oneBear.checkout();
-        assertEquals(5.35, ans, 0.005);
+        Bear testbear1 = new Bear(Stuffing.stuffing.DOWN);//$41
+        
+        double taxCost = oneBear.calculateTax(); //New Line of code
+        
+        Double ans = oneBear.getCost(testbear1); 
+        
+        ans += taxCost; //New Line of code
+        
+        assertEquals(42.07, ans, 0.5);
     }
 }
-*/
