4e186d27e97694ff5b3bbe7e38ca19b20732252e
shaun vulaj <svulaj@asu.edu>
2022-02-26 16:01:05 -0700

    This test is built off the previous test casegetCost_clothingTraversal() but now includes the additional edge coverage to cover the branch where the cost of a single bears NoiseMaker is now added to the cost of said bear

--- src/test/java/GivenWhiteBox.java
+++ src/test/java/GivenWhiteBox.java
@@ -77,6 +77,45 @@ public class GivenWhiteBox {
          assertEquals(71.07, ans, 0.5);
     }
     
+    /**
+     * This method tests node coverage --> Node 43 - 50.   -------> Fully covers all nodes.
+     * This method passes with flying colors indicating that the clothing
+     * 		clothing list traversal functionality work properly. 
+     * 		It show uses four pieces of clothing, on of which is discounted
+     * 		as described in the specification above the getCost() in BearWorkshop.java.
+     * 
+     * This test shows $30 being added to the cost of a single bear as opposed to $40
+     * which is the total additional cost of all 4 pieces of clothing had they 
+     * all been added to the bear.
+     * 
+     * NEW: This test is the same as the previous but now has edge coverage to 
+     * 		test whether or not the if-condition for adding the noiseMaker
+     * 		cost to the individual bears total cost. 
+     */
     
+    @Test
+    public void getCost_clothingTraversal_with_ifBranch() {
+        // One Student
+        oneBear = new BearWorkshop("AZ");
+        Bear testbear0 = new Bear(Stuffing.stuffing.DOWN);//$41
+
+        oneBear.addBear(testbear0);
+
+         Clothing cloth0 = new Clothing(10,"show r");
+         Clothing cloth1 = new Clothing(10,"show r");
+         Clothing cloth2 = new Clothing(10,"show r");
+         Clothing cloth3 = new Clothing(10,"show r");
+         
+         testbear0.clothing.add(cloth0);
+         testbear0.clothing.add(cloth1);
+         testbear0.clothing.add(cloth2);
+         testbear0.clothing.add(cloth3);
+         
+         NoiseMaker noise0 = new NoiseMaker();
+         testbear0.noisemakers.add(noise0);
+         
+         Double ans = oneBear.getCost(testbear0);
+         assertEquals(81.07, ans, 0.5);
+    }
     
 }
